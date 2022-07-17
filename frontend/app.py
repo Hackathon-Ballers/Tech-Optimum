2eeae17eaa4743fd92c3c00fd93eb49219eb336b
Joey Marcinowski <jmarcinowski@hillers.org>
2022-07-17 01:50:45 -0400

    sdsfji

---
+++ frontend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template('index.html')
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('charts.html')
+
+num = random.randint(0, 9999)
+@app.route('/refresh')
+def refresh():
+    return {'state': num}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
